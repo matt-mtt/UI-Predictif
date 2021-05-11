da888b8b5d7769212db15f7e62aaffc7444f5319
maria-lemes <61497174+maria-lemes@users.noreply.github.com>
2021-05-11 09:12:10 +0200

    Add files via upload

---
+++ src/main/webapp/java/Actions/Action.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actions;
+
+import javax.servlet.http.HttpServletRequest;
+import metier.service.Service;
+
+/**
+ *
+ * @author MariaLemes
+ */
+public abstract class Action {
+    
+    Service service;
+    
+    public Action(Service service){
+        this.service = service;
+    }
+    
+    public abstract void execute(HttpServletRequest request);
+    
+    
+}
