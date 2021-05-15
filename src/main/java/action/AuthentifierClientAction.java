1a79516793f31d0254aa49734cdf7bb278fd02ba
maria-lemes <madulemes10@gmail.com>
2021-05-15 17:16:53 +0200

    session

--- src/main/java/action/AuthentifierClientAction.java
+++ src/main/java/action/AuthentifierClientAction.java
@@ -7,7 +7,9 @@ package action;
 
 import com.google.gson.JsonObject;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import metier.modele.Client;
+import metier.modele.Employe;
 import metier.service.Service;
 
 /**
@@ -23,15 +25,25 @@ public class AuthentifierClientAction extends Action {
 
     @Override
     public void execute(HttpServletRequest request) {
+        
+        HttpSession session = request.getSession(true);
         String login = request.getParameter("login");
         String password = request.getParameter("password");
         System.out.println(login);
         System.out.println(password);
         Client c = service.authentifierClient(login, password);
         System.out.println(c);
+        Employe e = service.authentifierEmploye(login, password);
+        System.out.println(e);
 
         if(c!=null){
             request.setAttribute("client",c);
+            session.setAttribute("user", c.getId());
+            session.setAttribute("userType", "client");
+        }else if(e!=null){
+            request.setAttribute("employe",e);
+            session.setAttribute("user", e.getId());
+            session.setAttribute("userType", "employe");
         }
        
     
